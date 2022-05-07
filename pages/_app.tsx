9af230f7ad6f2089254fb91c112f6710b284998f
choidy180 <choidy180@naver.com>
2022-05-07 23:52:48 +0900

    commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import '../styles/global-style.ts';
+import Head from 'next/head';
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "../styles/global-style";
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Next-basics | Next</title>
+      </Head>
+      <GlobalStyle/>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-white h-screen flex flex-col justify-center items-center">
-      <h1 className="text-8xl">MAKERS</h1>
-      <img 
-        className="max-w-[300px] mt-7"
-        src="https://w.namu.la/s/b84c22572038c86c7d73ec2de8310af4d5d6e76d599f08db5c47755be58f1d4533bf939e6af8af2c0ad4836ef9bf4c1f534c3d8a5a958541089ee6c4d8f7b0a1db79b65792b845712a21ca835c69719200f77104f7d579aa8e650f20981ffce7fdfea8fb3a9863641d9f646518f72d73" 
-        alt="California" 
-      />
-      <h1 className="text-8xl mt-7">
-        PRIME<b className="text-9xl -mt-4 leading-3 text-green-900">.</b>
-      </h1>
-    </div>
+    <div>Next-basics</div>
   )
 }
 
